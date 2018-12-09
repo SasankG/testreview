b66ad48132ebd2996c00eaa77eeae27e8e38e489
SasankG <sasankgraju@gmail.com>
2018-12-08 20:26:33 -0500

    Updates

---
+++ precache-manifest.42af120585811d373aca7134953d613b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreview/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4821b6f895bbbf9c54df",
+    "url": "/testreview/static/js/runtime~main.4821b6f8.js"
+  },
+  {
+    "revision": "5cd416a2e72500aac5f4",
+    "url": "/testreview/static/js/main.5cd416a2.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/testreview/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "5cd416a2e72500aac5f4",
+    "url": "/testreview/static/css/main.ae82b334.chunk.css"
+  },
+  {
+    "revision": "801ea6d20b21e3dce4cea8d49075b8ef",
+    "url": "/testreview/index.html"
+  }
+];
